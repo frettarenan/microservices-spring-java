08377ab029b6bce01496de7521f7260277eb8c4d
renanfretta <renanfretta@vhintranet.com.br>
2021-02-13 18:31:06 -0300

    Criado projeto "cadastros-essenciais-microservice", criado a configuração básica do projeto com parent pom, ainda faltam criar as configurações para rodar o projeto

---
+++ cadastros-essenciais-microservice/src/main/java/com/br/renanfretta/cadastrosessenciais/CadastrosEssenciaisMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.br.renanfretta.cadastrosessenciais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastrosEssenciaisMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastrosEssenciaisMicroserviceApplication.class, args);
+	}
+
+}
