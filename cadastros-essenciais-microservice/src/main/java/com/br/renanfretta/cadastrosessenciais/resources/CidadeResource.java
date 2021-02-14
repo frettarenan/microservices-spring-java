6e01d894bdd0347bc203e894c230d1703fee813f
renanfretta <renanfretta@vhintranet.com.br>
2021-02-14 09:59:25 -0300

    Teste da parte dos serviços da entidade cidade pelo POSTMAN

--- cadastros-essenciais-microservice/src/main/java/com/br/renanfretta/cadastrosessenciais/resources/CidadeResource.java
+++ cadastros-essenciais-microservice/src/main/java/com/br/renanfretta/cadastrosessenciais/resources/CidadeResource.java
@@ -2,6 +2,8 @@ package com.br.renanfretta.cadastrosessenciais.resources;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -46,13 +49,13 @@ public class CidadeResource {
 	}
 	
 	@PostMapping
-	public ResponseEntity<Cidade> salvar(Cidade cidade) {
+	public ResponseEntity<Cidade> salvar(@Valid @RequestBody Cidade cidade) {
 		cidade = repository.save(cidade);
 		return ResponseEntity.status(HttpStatus.CREATED).body(cidade);
 	}
 	
 	@PutMapping
-	public ResponseEntity<Cidade> atualizar(Cidade cidade) {
+	public ResponseEntity<Cidade> atualizar(@Valid @RequestBody Cidade cidade) {
 		cidade = repository.save(cidade);
 		return ResponseEntity.status(HttpStatus.OK).body(cidade);
 	}
